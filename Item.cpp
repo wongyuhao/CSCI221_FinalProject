b8303f682a63309721cadff3c65881e05fb95824
Evirir <gojunxing@gmail.com>
2020-04-07 01:30:59 +0800

    Added item class and subclasses; Added Type abstract class
    - Type: Entity class without coordinates
    - Class relationship:
    
    Type:
    Item:
    -> WeaponItem: PlayerWeaponItem, CubeWeaponItem
    -> HealingItem
    -> MovementItem
    Note: For CubeWeaponItem, call only use(const Entity& location, vector<Player>& playerList)

--- Item.cpp
+++ Item.cpp
@@ -1,1 +1,50 @@
 #include "Item.h"
+
+string Item::getName() const {
+	return name;
+}
+
+int Item::getCost() const {
+	return cost;
+}
+
+int Item::getStat() const {
+	return stat;
+}
+
+void Item::buyItem(Player& player) const {
+	if(player.getCurrency() < cost){
+		cout << "Not enough currency." << endl;
+		return;
+	}
+	
+	player.addCurrency(-cost);
+	player.getEquippedItems()[getID()]++;
+	cout << "Successfully bought a(n) " << name << endl;
+}
+
+int WeaponItem::getRange() const {
+	return range;
+}
+
+void WeaponItem::use(Player& target) {
+	target.addHealthStat(-stat);
+}
+
+void CubeWeaponItem::use(const Entity& location, vector<Player>& playerList) {
+	for(Player player: playerList) {
+		if(	location.getPosX() - radius < player.getPosX() && player.getPosX() < location.getPosX() + radius && 
+			location.getPosY() - radius < player.getPosY() && player.getPosY() < location.getPosY() + radius)
+		{
+			player.addHealthStat(-stat);
+		}
+	}
+}
+
+void HealingItem::use(Player& target) {
+	target.addHealthStat(stat);
+}
+
+void MovementItem::use(Player& target) {
+	target.addMovementStat(stat);
+}
