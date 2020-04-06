44244c99f7918db5faa619b86c88812129c5cc8a
Evirir <gojunxing@gmail.com>
2020-04-07 01:24:01 +0800

    Made functions inline, changed include guard
    - Setter/Getter functions are now inline
    - Changed #pragma once to the more standard include guard

--- Entity.h
+++ Entity.h
@@ -1,12 +1,12 @@
-#pragma once
+#ifndef ENTITY_H
+#define ENTITY_H
+
 class Entity
 {
 private:
 	int posX;
 	int posY;
 	char ID;
-
-
 
 public:
 	Entity(char id, int x, int y):
@@ -23,30 +23,31 @@ public:
 	ID(' ')
 	{
 	
-	};
+	}
 
-	void setPosX(int x) {
+	inline void setPosX(int x) {
 		posX = x;
 	}
 
-	int getPosX() const {
+	inline int getPosX() const {
 		return posX;
 	}
 
-	void setPosY(int y) {
+	inline void setPosY(int y) {
 		posY = y;
 	}
 
-	int getPosY()const {
+	inline int getPosY()const {
 		return posY;
 	}
 
-	void setID(char id) {
+	inline void setID(char id) {
 		this->ID = id;
 	}
-	char getID()const {
+	inline char getID()const {
 		return ID;
 	}
 
 };
 
+#endif
