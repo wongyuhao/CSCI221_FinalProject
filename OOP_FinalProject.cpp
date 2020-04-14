a50b68ba961e64cb676ed7c75e3987a1095cd76f
Evirir <gojunxing@gmail.com>
2020-04-15 00:31:00 +0800

    Added item initialization from Items.txt

--- OOP_FinalProject.cpp
+++ OOP_FinalProject.cpp
@@ -1,4 +1,6 @@
 #include <iostream>
+#include <fstream>
+#include <cassert>
 #include <random>
 #include <ctime>
 
@@ -21,10 +23,46 @@ int main() {
 		playerList.push_back(Player(playerID));
 	}
 	
-	//initialize items
-	for (int i = 0; i < itemList.size(); i++) {
-		itemList[i].setID(i);
+	//DEBUGGING CODE: delete this before final release
+	/*playerList[0].setPosX(15); playerList[0].setPosY(15);
+	playerList[1].setPosX(15); playerList[1].setPosY(16);*/
+	
+	//create file stream to read item file
+	ifstream fin("Items.txt");
+	assert(fin);
+	
+	int currentID = 0;
+	string name, type;
+	int cost, energyCost, stat, durability, range, radius;
+	
+	while(getline(fin, name)) {
+		fin >> type;
+		if(type == "weaponP") {
+			fin >> cost >> energyCost >> stat >> durability >> range;
+			itemList.push_back(new PlayerWeaponItem(currentID, name, cost, energyCost, stat, durability, range));
+		}
+		else if(type == "weaponS") {
+			fin >> cost >> energyCost >> stat >> durability >> range >> radius;
+			itemList.push_back(new CubeWeaponItem(currentID, name, cost, energyCost, stat, durability, range, radius));
+		}
+		else if(type == "healing") {
+			fin >> cost >> stat;
+			itemList.push_back(new HealingItem(currentID, name, cost, stat));
+		}
+		else if(type == "energy") {
+			fin >> cost >> stat >> durability;
+			itemList.push_back(new EnergyItem(currentID, name, cost, stat, durability));
+		}
+		else {
+			cout << "Error when reading Items.txt: Invalid item type." << endl;
+			return 0;
+		}
+		
+		currentID++;
+		fin.ignore(2);
 	}
+	
+	fin.close();
 	
 	Display display;
 	
