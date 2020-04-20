c29895aa015afa4a0ca735b3db1be95fc726cace
Evirir <gojunxing@gmail.com>
2020-04-20 13:20:47 +0800

    Added MapUtil for adding buildings

---
+++ MapUtil.h
@@ -0,0 +1,28 @@
+#ifndef MAP_UTIL
+#define MAP_UTIL
+
+#include "Config.h"
+
+#include <cassert>
+#include <algorithm>
+using namespace std;
+
+//All printX() functions assigns the character c[0]/c[1] alternatively to gameMap[][] in the...
+
+class MapUtil {
+private:
+	static bool outOfBounds(const int X, const int Y);
+public:
+	//add buildings into gameMap using the functions below
+	static void addBuildings(char gameMap[MAPHEIGHT][MAPWIDTH]);
+	
+	//horizontal line (x, y1..y2)
+	static void printHorizontalLine(char gameMap[MAPHEIGHT][MAPWIDTH], const int x, const int y1, const int y2, const char c[3]);
+	//vertical line (x1..x2, y)
+	static void printVerticalLine(char gameMap[MAPHEIGHT][MAPWIDTH], const int y, const int x1, const int x2, const char c[3]);
+
+	//rectangle (x1..x2, y1..y2)
+	static void printRectangle(char gameMap[MAPHEIGHT][MAPWIDTH], const int x1, const int y1, const int x2, const int y2, const char c[3]);
+};
+
+#endif
